6cec925e8e1320791fcb34418bbc01e1bb82bc25
rjbx <rjbx@users.noreply.github.com>
2019-01-26 10:04:36 -0500

    fix: Revert changes to Java library algorithm
    Error was restored as a quotient of magnitude and precision
    Indeces dereferenced and saved for efficiency were removed for readability and to revert logical errors
    
    See also: f1267bd

--- calibrater/src/main/java/com/github/rjbx/calibrater/Calibrater.java
+++ calibrater/src/main/java/com/github/rjbx/calibrater/Calibrater.java
@@ -5,7 +5,7 @@ package com.github.rjbx.calibrater;
  */
 public class Calibrater {
 
-    public static final int STANDARD_PRECISION = 5;
+    public static final int STANDARD_PRECISION = 4;
     public static final double STANDARD_MAGNITUDE = .01d;
     
     /**
@@ -23,37 +23,35 @@ public class Calibrater {
         
         if (magnitude == 0 || percents.length < 2) 
             return false; // nothing to adjust
-        
-        double targetPercent = percents[targetIndex];
-        if ((targetPercent == 0d && magnitude < 0d)
-        || (targetPercent == 1d && magnitude > 0d))
+
+        if ((percents[targetIndex] == 0d && magnitude < 0d)
+        || (percents[targetIndex] == 1d && magnitude > 0d))
             return false; // percent outside adjustable limits
         
-        targetPercent += magnitude;
-        if (targetPercent >= 1d) { // adjusted percent is whole so rest must be zero
+        percents[targetIndex] += magnitude;
+        if (percents[targetIndex] >= 1d) { // adjusted percent is whole so rest must be zero
             percents[targetIndex] = 1d;
             for (int i = 0; i < percents.length; i++) if (targetIndex != i) percents[i] = 0d;
         } else {
 
             double offset = magnitude * -1;
-            if (targetPercent <= 0d) {
+            if (percents[targetIndex] <= 0d) {
                 percents[targetIndex] = 0d; // set to limit
-                offset += targetPercent; // restore unallocated offset
+                offset += percents[targetIndex]; // restore unallocated offset
             }
 
             int excluded = 1; // prevent further allocation after maxing out all elements
             double limit = magnitude > 0d ? 0d : 1d; // limit approached by offset percents
-            double error = 1d / Math.pow(10, precision);
+            double error = magnitude / Math.pow(10, precision);
 
             while (Math.abs(offset) >= Math.abs(error) && excluded <= percents.length) { // offset expended or exclusions maxed
                 double allocation = (offset / (percents.length - excluded)); // factor in exclusions on iterations
                 for (int i = 0; i < percents.length; i++) {
                     if (i != targetIndex && (percents[i] != 0d || magnitude < 0d)) { // ignore adjusted and exclude only once
                         percents[i] += allocation;
-                        double indexedPercent = percents[i];
                         offset -= allocation; // expend allocated for recalculating offset on iterations
-                        if (indexedPercent + error  < limit * -1) { // below limit within margin of error
-                            if (indexedPercent < 0d) offset += (indexedPercent + error); // restore unallocated offset
+                        if (percents[i] + error  < limit * -1) { // below limit within margin of error
+                            if (percents[i] < 0d) offset += (percents[i] + error); // restore unallocated offset
                             percents[i] = limit; // set to limit
                             excluded++; // decrease offset divisor for fewer allocations
                         }
