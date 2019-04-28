a4adf0965644a7c4b7a26b9bc9c051adddcd5dae
rjbx <rjbx@users.noreply.github.com>
2019-04-28 09:42:55 -0400

    chore: Remove annotation

--- sample/src/main/java/com/github/rjbx/sample/data/ColorData.java
+++ sample/src/main/java/com/github/rjbx/sample/data/ColorData.java
@@ -12,8 +12,6 @@ import java.util.Map;
 /**
  * Helper class for providing sample content for user interfaces created by
  * Android template wizards.
- * <p>
- * TODO: Replace all uses of this class before publishing your app.
  */
 public class ColorData {
 
